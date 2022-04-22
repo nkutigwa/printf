ff875e6e954ed7475d7fef241767ba83165d247a
nkutigwa <nkw802640@gmail.com>
2022-04-22 05:53:31 -0700

    adding puts.c file

---
+++ puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - Swaps integers wih pointers.
+ *
+ * @str: is a pointer to a char
+ *
+ * Return: Always 0.
+ */
+
+void _puts(const char *str)
+{
+int i = 0;
+
+while (*(str + i) != '\0')
+{
+_putchar(*(str + i));
+i++;
+}
+}
