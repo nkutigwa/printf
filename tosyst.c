5255d63425da808a47286a543092e7d56eb92466
nkutigwa <nkw802640@gmail.com>
2022-04-22 05:58:30 -0700

    tosyst.c file

---
+++ tosyst.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _tobinoct - Converts an usigned int to binary.
+ * @n: Passed number.
+ * @m: counter.
+ * @sys: system
+ * Return: Returns a binary converted number.
+**/
+int _tobinoct(unsigned int n, int m, unsigned int sys)
+{
+	if (n < sys)
+	{
+		_putchar(n + '0');
+		return (1);
+	}
+	m = _tobinoct(n / sys, m + 1, sys) + 1;
+	_putchar(n % sys + '0');
+	return (m);
+}
