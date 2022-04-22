38368a0579ff3008c977bbc8e4f24025c45ac3b2
nkutigwa <nkw802640@gmail.com>
2022-04-22 04:22:21 -0700

    adding test folder with main.c file

---
+++ test/main.c
@@ -0,0 +1,23 @@
+#include <limits.h>
+#include <stdio.h>
+#include <limits.h>
+#include "../main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/**_printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');
+	printf("This is a test: %z - %% - % - % c - \\ - \" - \' - %\n", 'A');**/
+
+	int n = _printf("%s\n", "");
+	int m = _printf("%r\n", "");
+
+	printf("%d, %d\n", n, m);
+
+	return (0);
+
+}
